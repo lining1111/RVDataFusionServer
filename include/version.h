bb158cbe09cff859c948b77c218158d3c1293f34
lining1111 <770677655@qq.com>
2022-02-17 11:12:37 +0800

    版本文件

---
+++ include/version.h
@@ -0,0 +1,10 @@
+#ifndef _VERSION_H
+#define _VERSION_H
+
+#define PROJECT_NAME "RVDataFusionServer"
+#define PROJECT_VERSION "0.0.1"
+/* #undef PROJECT_VERSION_MAJOR */
+/* #undef PROJECT_VERSION_MINOR */
+#define PROJECT_VERSION_PATCH "1"
+
+#endif //_VERSION_H
